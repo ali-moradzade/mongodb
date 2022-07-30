6f50357185fdbb26d45123dd7249603accd40a8d
Ali Moradzade <alimorizz1379@gmail.com>
2022-07-30 09:45:38 +0430

    adding half of 08-handling-relational-data

---
+++ 08-handling-relational-data/users/src/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const PostSchema = require('./post');
+
+const Scheme = mongoose.Schema;
+
+const UserSchema = new Scheme({
+    name: {
+        type: String,
+        required: [true, 'Name is required.'],
+        validate: {
+            validator: (name) => name.length > 2,
+            message: "Name must be longer than 2 characters."
+        },
+    },
+    postCount: Number,
+    posts: [
+        PostSchema
+    ],
+});
+
+const User = mongoose.model('user', UserSchema);
+module.exports = User;
+
