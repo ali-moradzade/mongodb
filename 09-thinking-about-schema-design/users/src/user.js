d8eb1f81e567cfc4bfd8c28e55776a5a7011388f
Ali Moradzade <alimorizz1379@gmail.com>
2022-07-30 12:41:47 +0430

    adding 09-thinking-about-schema-design

---
+++ 09-thinking-about-schema-design/users/src/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const PostSchema = require('./post');
+
+const Scheme = mongoose.Schema;
+
+const UserSchema = new Scheme({
+    name: {
+        type: String,
+        required: [true, 'Name is required.'],
+        validate: {
+            validator: (name) => name.length > 2,
+            message: "Name must be longer than 2 characters."
+        },
+    },
+    posts: [
+        PostSchema
+    ],
+    likes: Number,
+    blogPosts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'blogPost',
+        }
+    ],
+});
+
+UserSchema.virtual('postCount').get(function () {
+    return this.posts.length;
+});
+
+const User = mongoose.model('user', UserSchema);
+module.exports = User;
+
---
+++ 09-thinking-about-schema-design/users/test/test-helper.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb://localhost/users_test`);
+
+before((done) => {
+    mongoose.connection
+        .once('open', () => {
+            done();
+        })
+        .on('error', (error) => {
+            console.warn('Warning', error);
+        });
+});
+
+beforeEach((done) => {
+    const {users, blogposts, comments} = mongoose.connection.collections;
+
+    users.drop(() => {
+        comments.drop(() => {
+            blogposts.drop(() => {
+                done();
+            });
+        });
+    });
+});
