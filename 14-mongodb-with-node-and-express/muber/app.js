83a0569ae11b22990abfce71822df0fad72711b1
Ali Moradzade <alimorizz1379@gmail.com>
2022-08-06 11:43:10 +0430

    adding 14-mongodb-with-node-and-express, but the GET to /api/drivers to find location does not work

--- 14-mongodb-with-node-and-express/muber/app.js
+++ 14-mongodb-with-node-and-express/muber/app.js
@@ -3,11 +3,17 @@ const routes = require('./routes/routes');
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/muber');
+if (process.env.NODE_ENV !== 'test') {
+    mongoose.connect('mongodb://localhost/muber');
+}
 
 const app = express();
 
 app.use(bodyParser.json());
 routes(app);
+
+app.use((err, req, res, next) => {
+    res.status(422).send({error: err._message});
+})
 
 module.exports = app;
--- 14-mongodb-with-node-and-express/muber/controllers/drivers-controller.js
+++ 14-mongodb-with-node-and-express/muber/controllers/drivers-controller.js
@@ -5,12 +5,47 @@ module.exports = {
         res.send({hi: 'there!'});
     },
 
-    create(req, res) {
+    index(req, res, next) {
+        const {lng, lat} = req.query;
+
+        Driver.geoNear(
+            {
+                type: 'Point',
+                coordinates: [parseFloat(lng), parseFloat(lat)],
+            },
+            {
+                spherical: true,
+                maxDistance: 200000,
+            }
+        ).then(drivers => res.send(drivers))
+            .catch(next);
+    },
+
+    create(req, res, next) {
         const driverProps = req.body;
 
         Driver.create(driverProps)
             .then(driver => {
                 res.send(driver);
-            });
+            })
+            .catch(next);
     },
+
+    edit(req, res, next) {
+        const driverId = req.params.id;
+        const driverProps = req.body;
+
+        Driver.updateOne({_id: driverId}, driverProps)
+            .then(() => Driver.findById(driverId))
+            .then(driver => res.send(driver))
+            .catch(next);
+    },
+
+    delete(req, res, next) {
+        const driverId = req.params.id;
+
+        Driver.deleteOne({_id: driverId})
+            .then(driver => res.status(204).send(driver))
+            .catch(next);
+    }
 };
